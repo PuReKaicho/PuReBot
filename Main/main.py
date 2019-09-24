71f6711d8d6da8bf53d7afbac6506f1392672c01
PuReKaicho <andris.zibart@googlemail.com>
2019-09-24 20:55:57 +0200

    Update funktion grundlegend

--- Main/main.py
+++ Main/main.py
@@ -1,5 +1,5 @@
-import asyncio
 import discord
+from discord import Embed
 
 from PuReBot.Main import Static
 from PuReBot.Permissions import Secret
@@ -11,19 +11,42 @@ commands = {
 
     "help": help,
 
-
 }
 
+
 @client.event
-@asyncio
+async def on_ready():
+    print("Bot is logged in successfully. Running on servers:\n")
+    [(lambda s: print("  - %s (%s)" % (s.name, s.id)))(s) for s in client.guilds]
+    await client.change_presence(activity=discord.client.create_activity(name="Spiel"))
+    print(client.activity)
 
-def on_ready():
-    await client.send_message()
-
-def on_message(message):
+@client.event
+async def on_message(message):
     if message.content.startswith(Static.Prefix):
         invoke = message.content[len(Static.Prefix):].split(" ")[0]
         args = message.content.split(" ")[1:]
-        print("Invoke: %s\nARGS: %s" % (invoke, args.__str__)()[1:-1].replace("'"," "))
+        if commands.__contains__(invoke):
+            await commands.get(invoke).ex(args, message, client, invoke)
+        else:
+            await client.send_message(message.channel, embed=Embed(color=discord.Color.red(), descriotion=(
+                        "Der Befehl '%s' ist nicht gültig!!!" % invoke)))
+
+@client.event
+async def on_member_join(member):
+    await client.send_message(member, "css b,i Befehle des Bots:"
+                                   "**Invoke:  **                                    !"
+                                   "**Befehle: **                                   !help"
+                                   "**Nachrichten löschen:**            !clear"
+                                   "**Kick:**                                          !kick"
+                                   "**Youtube:**                                  !yt"
+                                   "** Regeln Musikplayer: **"
+                                           "Musik kann nur ab Vize/Admin hinzugefügt werden."
+                                           "Wenn man bestimmte Lieder hören möchte,"
+                                           "kann einem Admin eine PN mit den Liedern geschrieben werden."
+                                           "Die Maximale Anzahl an Liedern beträgt 15,"
+                                           "dementsprechend auch bei einer Playlist nur mit Maximal 15 Liedern."
+                                           "Die Lieder sollten eine Maximal Laufzeit von 15 Minuten nicht überschreiten.```")
+
 
 client.run(Secret.TOKEN)
